02565a4547d0c3669c01e8fe3c1f891a3dedc77b
James <tikx.batera@gmail.com>
2020-09-25 10:29:55 -0300

    Adding autologin / autologout and using localStorage user data

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,6 +19,7 @@ export interface IAuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
+  private _tokenExpirationTimer: any;
   private API_KEY = 'AIzaSyAX2TH71BPl3UYNRh4latgzo1GCPSbvpGM';
   private SIGNUP_URL =
     'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
@@ -59,6 +60,8 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   login(email: string, password: string) {
@@ -81,9 +84,47 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/auth']);
+    localStorage.removeItem('userData');
+    if (this._tokenExpirationTimer) {
+      clearTimeout(this._tokenExpirationTimer);
+    }
+    this._tokenExpirationTimer = null;
+  }
+
+  autoLogout(expirationDuration: number) {
+    this._tokenExpirationTimer = setTimeout(() => {
+      this.logout();
+    }, expirationDuration);
   }
 
   private handleError(errorRes: HttpErrorResponse) {
